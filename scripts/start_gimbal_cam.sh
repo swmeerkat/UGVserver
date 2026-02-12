4cd992fcd7cc4b209564d92fdc111f1db3403a98
Martin <martin.takats@t-online.de>
2026-02-12 10:31:46 +0100

    Optimization

--- scripts/start_gimbal_cam.sh
+++ scripts/start_gimbal_cam.sh
@@ -16,13 +16,16 @@
 #
 #gst-launch-1.0 v4l2src  extra-controls="c, auto_exposure=0" \
 #   device=/dev/video0 ! 'video/x-raw, format=YUY2, width=800, height=600, framerate=10/1' ! \
-#   nvvidconv ! x264enc tune=zerolatency speed-preset=ultrafast bitrate=16384 ! \
+#   nvvidconv ! x264enc tune=zerolatency speed-preset=ultrafast bitrate=8192 ! \
 #   rtph264pay pt=96 ! udpsink host=192.168.178.24 port=5000 sync=false -e > /dev/null 2>&1 &
 
 gst-launch-1.0 nvv4l2camerasrc  \
    device=/dev/video0 ! 'video/x-raw(memory:NVMM), format=YUY2, width=800, height=600, framerate=10/1' ! \
-   nvvidconv ! x264enc tune=zerolatency speed-preset=ultrafast bitrate=32768 ! \
-   rtph264pay pt=96 ! udpsink host=192.168.178.24 port=5000 sync=false -e > /dev/null 2>&1 &
+   nvvidconv ! \
+   x264enc tune=zerolatency speed-preset=ultrafast bitrate=8192 ! \
+   h264parse ! \
+   rtph264pay config-interval=1 pt=96 aggregate-mode=zero-latency ! \
+   udpsink host=192.168.178.24 port=5000 sync=false -e > /dev/null 2>&1 &
 echo $!
 
 # Test
